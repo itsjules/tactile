a806e5626f69401d8e6815f549179b8d88a086d2
Jules Podlipensky <58939950+itsjules@users.noreply.github.com>
2021-02-26 22:11:14 +0100

    counter works

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
 angleMode(DEGREES);
 let scale = 1.2;
-let scaleSmoothnes=0.2;
-
+let scaleSmoothnes=0.3;
+let hitArea={};
+let fade=0;
+let hitCounter=0;
 let longcane;
+let executed=false;
 
 function preload() {
   longcane = loadImage("assets/blindenstock_P.png");
 }
 window.preload = preload;
+
+function update(){
+  hitArea={left:window.width/2-window.width*0.05, right: window.width/2+window.width*0.05}
+}
+
 
 function longCaneHover() {
   push();
@@ -66,18 +74,37 @@ function longCaneHover() {
   pop();
 }
 
+
 function hitDetection(){
-    if(mouseX>window.width/2-window.width*0.05 && mouseX<window.width/2+window.width*0.05){
-        console.log("wupwup");
-        rectMode(CENTER);
-        rect(window.width/2,longcane.height,window.width*0.1)
+    if (mouseX>hitArea.left && mouseX<hitArea.right){
+        (fade>=255)? fade=255 : fade+=20;
+        if(!executed){
+        hitCount(); 
+        executed=true;
+        }
+    } else{
+        (fade<=0)? fade=0 : fade-=10; 
+        executed=false;
     }
+    rectMode(CENTER);
+    noStroke();
+    fill(86,194,232,fade);
+    rect(window.width/2,longcane.height,longcane.height*0.3)
 }
+
+function hitCount(){
+   hitCounter+=1;
+   return;
+}
+
+
 
 function draw() {
   background("grey");
+  update();
   hitDetection();
   longCaneHover();
+  console.log(hitCounter);
   
   // console.log(longcane);
   // console.log({mouseX,mouseY});
